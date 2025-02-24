f7c8a766ce52e096b582d6eaf974be3aa6850c7f
Nisha Bukke <sononenisha827@gmail.com>
2025-02-24 17:31:52 +0530

    namehippo responsive

--- script.js
+++ script.js
@@ -14,7 +14,41 @@ closePopup.addEventListener("click", () => {
   popup.style.display = "none";
 });
 
+// mobile menu
 
+$( document ).ready(function() {
+
+  $( ".cross" ).hide();
+  $( ".menu" ).hide();
+  $( ".canva_expander" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".canva_expander" ).hide();
+  $( ".cross" ).show();
+  });
+  });
+  
+  $( ".cross" ).click(function() {
+  $( ".menu" ).slideToggle( "slow", function() {
+  $( ".cross" ).hide();
+  $( ".canva_expander" ).show();
+  });
+  });
+  
+  });
+
+ // Close out sub menu
+ $('.sub__close').click(function(e) {
+  e.preventDefault();
+  
+  $(this).parent().parent().removeClass('is-active');
+});
+
+// Trigger sub menu
+$('.menu ul .nav__submenu').click(function(e) {
+  e.preventDefault();
+  
+  $(this).siblings().addClass('is-active');
+});
 
 
 
